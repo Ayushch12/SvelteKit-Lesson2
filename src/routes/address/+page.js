80ac44bfaf42bf527e05f4aa744a7a92eb2ab661
Ayush chalisee <ayushchalise12@gmail.com>
2023-10-30 22:47:41 +0100

    Server load

--- src/routes/address/+page.js
+++ src/routes/address/+page.js
@@ -8,3 +8,4 @@ export const load = async (loadEvent) => {
 
     return { addressList }; // corrected placement of curly braces
 };
+
--- src/routes/products/+page.server.js
+++ src/routes/products/+page.server.js
@@ -1,7 +1,7 @@
 
-export const load = async (loadEvent) => {
-    console.log("Load function called in page.js");
-    const {fetch} = loadEvent ;
+export const load = async (serverloadEvent) => {
+    console.log("Load function called in page.server.js");
+    const {fetch} = serverloadEvent ;
     const title = "List of available products";
     const response = await fetch("http://localhost:4000/products");
     const products = await response.json()
